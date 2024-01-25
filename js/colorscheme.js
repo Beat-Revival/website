c38cbc4c81ba2cea775a2f6d9ab0182b07b12df3
Loomeh <loombadoop@gmail.com>
2024-01-25 23:58:20 +0000

    bug fixes

--- js/colorscheme.js
+++ js/colorscheme.js
@@ -3,21 +3,29 @@ console.log("Theme switcher initialised");
 function setDarkTheme(darkTheme) {
     if (darkTheme) {
         document.querySelectorAll(".desc").forEach((element) => {
-            element.classList.add("dark");
+            if (element.classList) {
+                element.classList.add("dark");
+            }
         });
-        document.body.classList.add("dark");
+        if (document.body.classList) {
+            document.body.classList.add("dark");
+        }
     } else {
         document.querySelectorAll(".desc").forEach((element) => {
-            element.classList.remove("dark");
+            if (element.classList) {
+                element.classList.remove("dark");
+            }
         });
-        document.body.classList.remove("dark");
+        if (document.body.classList) {
+            document.body.classList.remove("dark");
+        }
     }
-
 }
 
 // Check user's system preferences
-const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-// Use window load event to ensure the DOM is fully loaded before executing the script
-window.addEventListener('load', () => {
+const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Use DOMContentLoaded event to ensure the DOM is fully loaded before executing the script
+document.addEventListener('DOMContentLoaded', () => {
     setDarkTheme(prefersDarkMode);
 });
